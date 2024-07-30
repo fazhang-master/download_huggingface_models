22abc12d21dd0778e0cc7487ab50956bef3c17d3
fazhang-master <fzhangvip@163.com>
2024-07-30 21:46:45 +0800

    first commit

---
+++ download_huggingface_models.py
@@ -0,0 +1,11 @@
+import os
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+from huggingface_hub import snapshot_download
+
+
+# 替换为自己的存储路径
+model_path = "/root/storm/models"
+snapshot_download( \
+            repo_id="sentence-transformers/paraphrase-MiniLM-L6-v2",\
+            local_dir=model_path,\
+            max_workers=8)
